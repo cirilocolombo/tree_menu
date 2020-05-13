48d8dc328bab1788f232b75b4697899e0f4057e5
Cirilo Colombo <cirilo.colombo@gmail.com>
2020-05-13 16:30:44 -0300

    Update code patterns

--- components/tree.js
+++ components/tree.js
@@ -1,46 +1,41 @@
 export default function(data) {
-  // pega a tag principal que irá receber o menu
-  const tree = document.querySelector('nav#tree')
+	// pega a tag principal que irá receber o menu
+	const tree = document.querySelector('nav#tree')
 
-  // recebe toda a arvore de elementos
-  const menu = document.createElement('ul')
+	// recebe toda a arvore de elementos
+	const menu = document.createElement('ul')
 
-  const firstLevel = data.filter(item => !item.parent)
-  const getFirstLis = firstLevel.map(buildTree) // retorno novo array com li's
-  getFirstLis.forEach( li => menu.append(li)) // adicionar li's ao menu
+	const firstLevel = data.filter(item => !item.parent)
+	const getFirstLis = firstLevel.map(buildTree) // retorno novo array com li's
+	getFirstLis.forEach(li => menu.append(li)) // adicionar li's ao menu
 
+	function buildTree(item) {
+		// primeiro elemento
+		const li = document.createElement('li')
+		li.innerHTML = item.name
 
-  function buildTree(item) {
-      
-      // primeiro elemento
-      const li = document.createElement('li')
-      li.innerHTML = item.name
+		const children = data.filter(child => child.parent === item.id)
 
+		if (children.length > 0) {
+			//adiciona um click para os parents
+			li.addEventListener('click', event => {
+				event.stopPropagation()
+				event.target.classList.toggle('open')
+			})
 
-      const children = data.filter(child => child.parent === item.id)
+			// adiciona uma classe identificadora de que tem filhos
+			li.classList.add('has-children')
 
-      if(children.length > 0) {
+			// constroi os filhos
+			const subMenu = document.createElement('ul')
+			children.map(buildTree).forEach(li => subMenu.append(li))
+			li.append(subMenu)
+		}
 
-          //adiciona um click para os parents
-          li.addEventListener('click', event => {
-              event.stopPropagation()
-              event.target.classList.toggle('open')
-          })
+		// adicionar os elements ao menu
+		return li
+	}
 
-          // adiciona uma classe identificadora de que tem filhos
-          li.classList.add('has-children')
-
-          // constroi os filhos
-          const subMenu = document.createElement('ul')
-          children.map(buildTree)
-          .forEach(li => subMenu.append(li))
-          li.append(subMenu)
-      }
-      
-      // adicionar os elements ao menu
-      return li
-  }
-
-  // adiciona o menu no HTML
-  tree.append(menu)
+	// adiciona o menu no HTML
+	tree.append(menu)
 }
